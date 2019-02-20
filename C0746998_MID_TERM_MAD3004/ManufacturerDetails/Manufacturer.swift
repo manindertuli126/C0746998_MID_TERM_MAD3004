e708655abf267a2388399fccd77e088a04ca013b
MacStudent <macstudent@126-iMac05.local>
2019-02-20 18:32:05 -0500

    Created Folder with separate files for each module.

---
+++ C0746998_MID_TERM_MAD3004/ManufacturerDetails/Manufacturer.swift
@@ -0,0 +1,9 @@
+//
+//  Manufacturer.swift
+//  C0746998_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-02-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
